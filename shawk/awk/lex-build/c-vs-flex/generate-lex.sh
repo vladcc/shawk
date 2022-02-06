c4c3b6a7c10a4fe97d57b44f6b057270a23f816c
Vladimir <vld.dinev@gmail.com>
2022-02-06 19:52:09 +0100

    lex-build - c-vs-flex/generate-lex.sh now uses G_AWK

--- shawk/awk/lex-build/c-vs-flex/generate-lex.sh
+++ shawk/awk/lex-build/c-vs-flex/generate-lex.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+G_AWK="${G_AWK:-awk}"
+
 function main
 {
-	pushd $(dirname $(realpath $0))
+	pushd "$(dirname $(realpath $0))"
+
+	echo "$0 generating with $G_AWK"
 	
 	if [ "$1" = "awk" ]; then
 		generate_awk
@@ -15,15 +19,15 @@ function main
 
 function generate_awk
 {
-	awk -f ../lex-first.awk lex.lb |
-	awk -f ../lex-awk.awk > ./awk/inc_lex.awk
+	$G_AWK -f../lex-first.awk lex.lb |
+	$G_AWK -f../lex-awk.awk > ./awk/inc_lex.awk
 }
 
 function generate_c
 {
-	awk -f ../lex-first.awk lex.lb |
-	awk -f ../lex-c.awk -vKeywords="$1" |
-	awk -vType="$1" '/<lex_header>/, /<\/lex_header>/ {print $0 > "./lex.h"}
+	$G_AWK -f../lex-first.awk lex.lb |
+	$G_AWK -f../lex-c.awk -vKeywords="$1" |
+	$G_AWK -vType="$1" '/<lex_header>/, /<\/lex_header>/ {print $0 > "./lex.h"}
 	/<lex_source>/, /<\/lex_source>/ {print $0 > sprintf("./lex_%s.c", Type)}'
 }
 
